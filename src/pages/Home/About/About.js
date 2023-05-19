5e5d967c1bc0f72f523277fac986f66eeb0f753e
R7558 <rupsanaworld1004@gmail.co>
2023-05-19 23:39:59 +0600

    added about

---
+++ src/pages/Home/About/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import about from '../../../assets/image/about.png'
+const About = () => {
+    return (
+        <div>
+            <div className="hero min-h-screen ">
+  <div className="hero-content flex-col lg:flex-row">
+    <img src={about} className="max-w-sm rounded-lg shadow-2xl" alt='' />
+    <div>
+      <h1 className="text-5xl font-bold">Box Office News!</h1>
+      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default About;
--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Services from '../Sevice/Services';
 import Offer from '../Offer/Offer';
+import About from '../About/About';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <Services></Services>
             <Offer></Offer>
+            <About></About>
         </div>
     );
 };
