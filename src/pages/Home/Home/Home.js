082c6d9d3e1da09c9df95bd525284472acbadea4
R7558 <rupsanaworld1004@gmail.co>
2023-05-19 23:31:22 +0600

    added offer

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Services from '../Sevice/Services';
+import Offer from '../Offer/Offer';
 
 const Home = () => {
     return (
         <div className='mx-20'>
             <Banner></Banner>
             <Services></Services>
+            <Offer></Offer>
         </div>
     );
 };
