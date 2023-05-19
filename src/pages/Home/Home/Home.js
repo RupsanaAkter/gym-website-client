9b2f760f31a00b7a882ff3b77c89d2f6a08a7432
R7558 <rupsanaworld1004@gmail.co>
2023-05-19 23:22:54 +0600

    added service

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Services from '../Sevice/Services';
 
 const Home = () => {
     return (
         <div className='mx-20'>
             <Banner></Banner>
+            <Services></Services>
         </div>
     );
 };
---
+++ src/pages/Home/Sevice/Services.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Service from './Service';
+import emage1 from "../../../assets/icons/emage1.png"
+import emage2 from "../../../assets/icons/emage2.png"
+import emage3 from "../../../assets/icons/emage3.png"
+import emage4 from "../../../assets/icons/emage4.png"
+const Services = () => {
+    const servicesData = [
+        {
+            id: 1,
+            name: 'Weight Lifting',
+            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+            img: emage2
+        },
+        {
+            id: 2,
+            name: 'Specific muscle',
+            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+            img: emage1
+        },
+        {
+            id: 3,
+            name: 'Flex Your Muscle',
+            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+            img: emage3
+        },
+        {
+            id: 4,
+            name: 'Cardio Exercise',
+            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+            img: emage4 
+        }
+    ]
+    return (
+        <div className='mt-16'>
+        <div className='text-center'>
+            <h3 className='text-xl font-bold text-primary uppercase'>Our Services</h3>
+            <h2 className='text-3xl'>Services We Provide</h2>
+        </div>
+        <div className='grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
+            {
+                servicesData.map(service => <Service
+                    key={service.id}
+                    service={service}
+                ></Service>)
+            }
+        </div>
+    </div>
+    );
+};
+
+export default Services;
